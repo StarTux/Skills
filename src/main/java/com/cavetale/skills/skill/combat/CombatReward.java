9e0ec8565db48fb352609add8c0ce73f4a78a3b1
StarTux <ths.loy@googlemail.com>
2022-07-15 23:03:37 +0200

    Specify the values

--- src/main/java/com/cavetale/skills/skill/combat/CombatReward.java
+++ src/main/java/com/cavetale/skills/skill/combat/CombatReward.java
@@ -16,65 +16,65 @@ public final class CombatReward {
 
     static {
         // Regular
-        reward(1, 1.0, DROWNED);
-        reward(1, 1.0, HUSK);
-        reward(1, 1.0, SKELETON);
-        reward(1, 1.0, STRAY);
-        reward(1, 1.0, ZOMBIE);
-        reward(1, 1.0, ZOMBIE_VILLAGER);
+        reward(5, 20.0, DROWNED);
+        reward(5, 20.0, HUSK);
+        reward(5, 20.0, SKELETON);
+        reward(5, 20.0, STRAY);
+        reward(5, 20.0, ZOMBIE);
+        reward(5, 20.0, ZOMBIE_VILLAGER);
 
         // Slimes
-        reward(1, 1.0, SLIME);
-        reward(1, 1.0, MAGMA_CUBE);
+        reward(3, 20.0, SLIME);
+        reward(3, 20.0, MAGMA_CUBE);
 
         // Tiny
-        reward(1, 1.0, VEX);
-        reward(1, 1.0, SILVERFISH);
-        reward(1, 1.0, ENDERMITE);
+        reward(1, 20.0, VEX);
+        reward(1, 20.0, SILVERFISH);
+        reward(1, 20.0, ENDERMITE);
 
         // Spiders
-        reward(2, 1.0, CAVE_SPIDER);
-        reward(2, 1.0, SPIDER);
+        reward(5, 20.0, CAVE_SPIDER);
+        reward(5, 20.0, SPIDER);
 
         // Overworld hard
-        reward(3, 1.0, CREEPER);
-        reward(3, 1.0, ENDERMAN);
-        reward(3, 1.0, PHANTOM);
+        reward(10, 20.0, CREEPER);
+        reward(15, 20.0, ENDERMAN);
+        reward(15, 20.0, PHANTOM);
 
         // Illagers
-        reward(3, 1.0, EVOKER);
-        reward(3, 1.0, ILLUSIONER);
-        reward(3, 1.0, PILLAGER);
-        reward(3, 1.0, VINDICATOR);
-        reward(3, 1.0, WITCH);
-        reward(10, 1.0, RAVAGER);
+        reward(15, 50.0, EVOKER);
+        reward(15, 50.0, ILLUSIONER);
+        reward(15, 50.0, PILLAGER);
+        reward(15, 50.0, VINDICATOR);
+        reward(15, 50.0, WITCH);
+        reward(50, 100.0, RAVAGER);
 
         // Pigs
-        reward(3, 1.0, HOGLIN);
-        reward(3, 1.0, PIGLIN);
-        reward(3, 1.0, ZOGLIN);
-        reward(3, 1.0, ZOMBIFIED_PIGLIN);
-        reward(3, 1.0, PIGLIN_BRUTE);
+        reward(15, 20.0, HOGLIN);
+        reward(15, 20.0, ZOGLIN);
+        reward(1, 1.0, PIGLIN);
+        reward(1, 1.0, ZOMBIFIED_PIGLIN);
+        reward(15, 20.0, PIGLIN_BRUTE);
 
         // Situational
-        reward(5, 1.0, BLAZE);
-        reward(5, 1.0, GHAST);
-        reward(5, 1.0, GUARDIAN);
-        reward(5, 1.0, SHULKER);
-        reward(5, 1.0, WITHER_SKELETON);
+        reward(20, 50.0, BLAZE);
+        reward(20, 50.0, GHAST);
+        reward(20, 50.0, GUARDIAN);
+        reward(20, 50.0, SHULKER);
+        reward(20, 50.0, WITHER_SKELETON);
 
         // Bosses
-        reward(10, 1.0, ELDER_GUARDIAN);
-        reward(20, 1.0, WARDEN);
-        reward(50, 1.0, ENDER_DRAGON);
-        reward(50, 1.0, WITHER);
+        reward(50, 100.0, ELDER_GUARDIAN);
+        reward(100, 500.0, WARDEN);
+        reward(100, 500.0, ENDER_DRAGON);
+        reward(100, 500.0, WITHER);
 
         // Removed
         reward(1, 1.0, GIANT);
     }
 
-    private static void reward(final int sp, final double moneyFactor, EntityType type) {
-        REWARDS.put(type, new CombatReward(type, sp * 5, 5.0 * (double) sp * moneyFactor));
+    private static void reward(final int sp, final double money, EntityType type) {
+        REWARDS.put(type, new CombatReward(type, sp, money));
     }
 
     public static CombatReward combatReward(Entity entity) {
