938179df95fb8ceda2a51365a1b09c9ce5f0d9df
StarTux <ths.loy@googlemail.com>
2023-03-11 19:37:59 +0100

    Miner sight works on ores

--- src/main/java/com/cavetale/skills/skill/mining/MinerSightTalent.java
+++ src/main/java/com/cavetale/skills/skill/mining/MinerSightTalent.java
@@ -47,7 +47,7 @@ public final class MinerSightTalent extends Talent implements Listener {
         Player player = event.getPlayer();
         if (!isPlayerEnabled(player)) return;
         Block block = event.getBlock();
-        if (!MiningSkill.anyStone(block)) return;
+        if (!MiningSkill.anyStone(block) && !MiningSkill.metalOre(block) && !MiningSkill.gemOre(block)) return;
         final ItemStack item = player.getInventory().getItemInMainHand();
         if (item == null || !MaterialTags.PICKAXES.isTagged(item.getType())) return;
         PotionEffect nightVision = player.getPotionEffect(PotionEffectType.NIGHT_VISION);
