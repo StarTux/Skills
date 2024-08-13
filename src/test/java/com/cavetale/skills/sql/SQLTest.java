9b317502972f5f7aab16161964477d320dbe2d2a
StarTux <ths.loy@gmail.com>
2024-08-13 10:44:27 +0200

    Disable SQL test

--- src/test/java/com/cavetale/skills/sql/SQLTest.java
+++ src/test/java/com/cavetale/skills/sql/SQLTest.java
@@ -1,10 +1,8 @@
 package com.cavetale.skills.sql;
 
 import com.winthier.sql.SQLDatabase;
-import org.junit.Test;
 
 public final class SQLTest {
-    @Test
     public void testTalentTypes() {
         System.out.println(SQLDatabase.testTableCreation(SQLPlayer.class));
         System.out.println(SQLDatabase.testTableCreation(SQLSkill.class));
